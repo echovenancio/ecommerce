f3fdb2ffc1c3bccafadb7154c6bbe9204dc8e067
echovenancio <venancio@local>
2025-04-20 11:13:47 -0300

    add product and sale to DefaultContext

--- Ro.DevTest/RO.DevTest.Persistence/DefaultContext.cs
+++ Ro.DevTest/RO.DevTest.Persistence/DefaultContext.cs
@@ -10,10 +10,25 @@ public class DefaultContext : IdentityDbContext<User> {
 
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
 
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Sale> Sales => Set<Sale>();
+
     protected override void OnModelCreating(ModelBuilder builder) {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
 
         base.OnModelCreating(builder);
+
+        builder.Entity<Sale>()
+            .HasOne(s => s.Product)
+            .WithMany()
+            .HasForeignKey(s => s.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Sale>()
+            .HasOne(s => s.User)
+            .WithMany()
+            .HasForeignKey(s => s.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
